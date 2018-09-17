fcbf244a929291c84def6d16b2e5fba0c6a6ee2c
Shirley <shirley@Shirleys-MacBook-Pro.local>
2018-09-17 14:40:39 +1200

    R Vector 1st version

---
+++ R Learning Vector.R
@@ -0,0 +1,79 @@
+# Define the variable vegas
+vegas <- "Go!"
+
+numeric_vector <- c(1, 10, 49)
+character_vector <- c("a", "b", "c")
+
+# Complete the code for boolean_vector
+boolean_vector <-c(TRUE,FALSE,TRUE)
+
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <- c(-24, -50, 100, -350, 10)
+
+# Assign days as names of poker_vector
+names(poker_vector) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+# Assign days as names of roulette_vector
+names(roulette_vector)<- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+# The variable days_vector
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+# Assign the names of the day to roulette_vector and poker_vector
+names(poker_vector) <- days_vector  
+names(roulette_vector) <- days_vector
+
+A_vector <- c(1, 2, 3)
+B_vector <- c(4, 5, 6)
+
+# Take the sum of A_vector and B_vector
+total_vector <- c(5, 7, 9)
+
+# Print out total_vector
+total_vector
+
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Total winnings with poker
+total_poker <- sum(poker_vector)
+
+# Total winnings with roulette
+total_roulette <-  sum(roulette_vector)
+
+# Total winnings overall
+total_week <- total_roulette+total_poker
+
+# Print out total_week
+total_week
+
+# Check if you realized higher total gains in poker than in roulette 
+total_poker>total_roulette
+
+# Define a new variable based on a selection
+poker_wednesday <- poker_vector[3]
+
+# Define a new variable based on a selection
+poker_midweek <- poker_vector[c(2,3,4)]
+
+# Define a new variable based on a selection
+roulette_selection_vector <- roulette_vector[c(2:5)]
+
+# Select poker results for Monday, Tuesday and Wednesday
+poker_start <- poker_vector[c("Monday", "Tuesday", "Wednesday")] 
+
+# Calculate the average of the elements in poker_start
+mean(poker_start)
+
+## Which days did you make money on poker?
+selection_vector <- poker_vector>0
+
+# Print out selection_vector
+selection_vector
