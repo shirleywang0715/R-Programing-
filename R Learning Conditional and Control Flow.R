411a46b83055d72178d43bac6bd837805ffc535f
Shirley <shirley@Shirleys-MacBook-Pro.local>
2018-11-20 11:22:09 +1300

    Update

--- R Learning Conditional and Control Flow.R
+++ R Learning Conditional and Control Flow.R
@@ -61,3 +61,81 @@ linkedin>=12 | facebook>=12
 
 # When is views between 11 (exclusive) and 14 (inclusive)?
 views >11 &views <=14
+
+# li_df is pre-loaded in your workspace
+
+# Select the second column, named day2, from li_df: second
+second <- li_df$day2
+
+# Build a logical vector, TRUE if value in second is extreme: extremes
+extremes <- second > 25 | second < 5
+
+# Count the number of TRUEs in extremes
+sum(extremes)
+
+# Solve it with a one-liner
+sum(li_df$day2 > 25 | li_df$day2 < 5)
+
+# Variables related to your last day of recordings
+medium <- "LinkedIn"
+num_views <- 14
+
+# Examine the if statement for medium
+if (medium == "LinkedIn") {
+  print("Showing LinkedIn information")
+}
+
+# Write the if statement for num_views
+if(num_views>15){
+  print("You're popular!")
+}
+
+# Control structure for medium
+if (medium == "LinkedIn") {
+  print("Showing LinkedIn information")
+}else{
+  print("Unknown medium")
+}
+
+# Control structure for num_views
+if (num_views > 15) {
+  print("You're popular!")
+}else{
+  print("Try to be more visible!" )
+}
+
+# Control structure for medium
+if (medium == "LinkedIn") {
+  print("Showing LinkedIn information")
+} else if (medium == "Facebook") {
+  # Add code to print correct string when condition is TRUE
+  print("Showing Facebook information")
+} else {
+  print("Unknown medium")
+}
+
+# Control structure for num_views
+if (num_views > 15) {
+  print("You're popular!")
+} else if (num_views <= 15 & num_views > 10) {
+  # Add code to print correct string when condition is TRUE
+  print("Your number of views is average")
+} else {
+  print("Try to be more visible!")
+}
+
+# Variables related to your last day of recordings
+li <- 15
+fb <- 9
+
+# Code the control-flow construct
+if (li>=15 & fb>=15) {
+  sms <- 2 * (li + fb)
+} else if (li<10 & fb<10) {
+  sms <- 0.5 * (li + fb)
+} else {
+  sms <- li+fb
+}
+
+# Print the resulting sms to the console
+sms
