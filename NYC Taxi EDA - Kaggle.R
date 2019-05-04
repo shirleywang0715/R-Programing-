469b2b28a0c70f324da2d43b9da56cce18296c03
Shirley <shirley@Shirleys-MacBook-Pro.local>
2019-05-04 17:28:42 +1200

    visualisation

--- NYC Taxi EDA - Kaggle.R
+++ NYC Taxi EDA - Kaggle.R
@@ -1,3 +1,4 @@
+#Load libraries and helper functions
 library('ggplot2') # visualisation
 library('scales') # visualisation
 library('grid') # visualisation
@@ -63,9 +64,134 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
   }
 }
 
+#Load data
 train <- as.tibble(fread('/Users/shirley/Documents/Data Learning/R Dataset/nyc-taxi-trip-duration/train.csv'))
 test <- as.tibble(fread('/Users/shirley/Documents/Data Learning/R Dataset/nyc-taxi-trip-duration/test.csv'))
 sample_submit <- as.tibble(fread('/Users/shirley/Documents/Data Learning/R Dataset/nyc-taxi-trip-duration/sample_submission.csv'))
 
+#File structure and content
 summary(train)
-  
+glimpse(train)
+
+summary(test)
+glimpse(test)
+
+#Missing values
+sum(is.na(train))
+sum(is.na(test))
+
+#Combining train and test
+combine <- bind_rows(train %>% mutate(dest="train"),
+                     test %>% mutate(dest="test",
+                                     dropoff_datetime = NA,
+                                     trip_duration=NA))
+combine <- combine %>% mutate(dest=factor(dset))
+
+#Reformating features
+train <- train %>%
+  mutate(pickup_datetime = ymd_hms(pickup_datetime),
+         dropoff_datetime = ymd_hms(dropoff_datetime),
+         vendor_id = factor(vendor_id),
+         passenger_count = factor(passenger_count))
+
+#Consistency check
+train %>%
+  mutate(check = abs(int_length(interval(dropoff_datetime,pickup_datetime)) + trip_duration) > 0) %>%
+  select(check, pickup_datetime, dropoff_datetime, trip_duration) %>%
+  group_by(check) %>%
+  count()
+
+#Individual feature visualisations
+train %>%
+  arrange(desc(trip_duration)) %>%
+  select(trip_duration, pickup_datetime, dropoff_datetime, everything()) %>%
+  head(10)
+
+p1 <- train %>%
+  ggplot(aes(pickup_datetime)) +
+  geom_histogram(fill = "red", bins = 120) +
+  labs(x = "Pickup dates")
+
+p2 <- train %>%
+  ggplot(aes(dropoff_datetime)) +
+  geom_histogram(fill = "blue", bins = 120) +
+  labs(x = "Dropoff dates")
+
+layout <- matrix(c(1,2),2,1,byrow=FALSE)
+multiplot(p1, p2, layout=layout)
+
+train %>%
+  filter(pickup_datetime > ymd("2016-01-20") & pickup_datetime < ymd("2016-02-10")) %>%
+  ggplot(aes(pickup_datetime)) +
+  geom_histogram(fill = "red", bins = 120)
+
+p1 <- train %>%
+  group_by(passenger_count) %>%
+  count() %>%
+  ggplot(aes(passenger_count, n, fill = passenger_count)) +
+  geom_col() +
+  scale_y_sqrt() +
+  theme(legend.position = "none")
+
+p2 <- train %>%
+  ggplot(aes(vendor_id, fill = vendor_id)) +
+  geom_bar() +
+  theme(legend.position = "none")
+
+p3 <- train %>%
+  ggplot(aes(store_and_fwd_flag)) +
+  geom_bar() +
+  theme(legend.position = "none") +
+  scale_y_log10()
+
+p4 <- train %>%
+  mutate(wday = wday(pickup_datetime, label = TRUE)) %>%
+  group_by(wday, vendor_id) %>%
+  count() %>%
+  ggplot(aes(wday, n, colour = vendor_id)) +
+  geom_point(size = 4) +
+  labs(x = "Day of the week", y = "Total number of pickups") +
+  theme(legend.position = "none")
+
+p5 <- train %>%
+  mutate(hpick = hour(pickup_datetime)) %>%
+  group_by(hpick, vendor_id) %>%
+  count() %>%
+  ggplot(aes(hpick, n, color = vendor_id)) +
+  geom_point(size = 4) +
+  labs(x = "Hour of the day", y = "Total number of pickups") +
+  theme(legend.position = "none")
+
+layout <- matrix(c(1,2,3,4,5,5),3,2,byrow=TRUE)
+multiplot(p1, p2, p3, p4, p5, layout=layout)
+
+p1 <- 1; p2 <- 1; p3 <- 1; p4 <- 1; p5 <- 1
+
+train %>%
+  group_by(passenger_count) %>%
+  count()
+
+train %>%
+  group_by(store_and_fwd_flag) %>%
+  count()
+
+p1 <- train %>%
+  mutate(hpick = hour(pickup_datetime),
+         Month = factor(month(pickup_datetime, label = TRUE))) %>%
+  group_by(hpick, Month) %>%
+  count() %>%
+  ggplot(aes(hpick, n, color = Month)) +
+  geom_line(size = 1.5) +
+  labs(x = "Hour of the day", y = "count")
+
+p2 <- train %>%
+  mutate(hpick = hour(pickup_datetime),
+         wday = factor(wday(pickup_datetime, label = TRUE))) %>%
+  group_by(hpick, wday) %>%
+  count() %>%
+  ggplot(aes(hpick, n, color = wday)) +
+  geom_line(size = 1.5) +
+  labs(x = "Hour of the day", y = "count")
+
+layout <- matrix(c(1,2),2,1,byrow=FALSE)
+multiplot(p1, p2, layout=layout)
